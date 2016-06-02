667ad659805c2ba3f5c3a7b955be489f97f5d2e0
chris <chris.ross@7digital.com>
2016-06-02 11:14:06 +0100

    Converts 5 to v

--- test/test_roman_numerals.rb
+++ test/test_roman_numerals.rb
@@ -4,10 +4,11 @@ require_relative '../lib/roman_numerals'
 
 describe 'roman_numerals' do
   it 'converts' do
-    rules = {1 => 'i', 4 => 'iv'}
+    rules = {1 => 'i', 4 => 'iv', 5 => 'v'}
     RomanNumerals.new(rules).convert(1).must_equal 'i'
     RomanNumerals.new(rules).convert(2).must_equal 'ii'
     RomanNumerals.new(rules).convert(3).must_equal 'iii'
     RomanNumerals.new(rules).convert(4).must_equal 'iv'
+    RomanNumerals.new(rules).convert(5).must_equal 'v'
   end
 end
